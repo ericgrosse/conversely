9329227facb98065975b2c95203d0cf31b30c9c3
Eric <ericgrosse1@gmail.com>
2020-10-29 08:42:27 -0400

    A message now appears when a user connects or disconnects from the app

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  io.emit('chat message', 'a user connected');
+  
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', () => {
+    io.emit('chat message', 'a user  disconnected');
   });
 });
 
